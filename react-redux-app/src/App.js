360751edc65fe0cbd46f86fa96d4320ce25d1349
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-12-12 12:42:57 -0800

    Commit 3: App.js set up

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import axios from 'axios';
+import PokeList from './components/PokeList';
+
 function App() {
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon")
+    .then((res) => {
+      console.log(res);
+    });
+
   return (
     <div className="App">
-
+      <PokeList />
     </div>
   );
 }
